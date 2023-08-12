c54e0da107174bd9394a6b02020469a958834c57
dhirajapp <dhirajkumar31298@gmail.com>
2023-08-12 23:12:50 +0530

    project setup done

---
+++ SpringAOPDemo/src/main/java/com/dhirajapp/BeanConfig.java
@@ -0,0 +1,10 @@
+package com.dhirajapp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.dhirajapp")
+public class BeanConfig {
+
+}
